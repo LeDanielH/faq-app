73248640127ddd926066f2c83d37efbca6df79e4
Daniel <daniel.hoksza@gmail.com>
2017-07-19 23:56:08 +0200

    got the damn slider working!

--- devel/assets/_scripts/components/slider.js
+++ devel/assets/_scripts/components/slider.js
@@ -1,71 +1,39 @@
 const Slider = {
-	slides: document.querySelectorAll('.faq__slider-item'),
-	titles: document.querySelectorAll('.faq__title'),
 	transformIncrement: 110,
-	transformBase: 120,
 	currentIndex: 0,
+	previousCount: 0,
+	nextCount: 0,
+	elements: function () {
+        this.slides = document.querySelectorAll('.faq__slider-item');
+		this.titles = document.querySelectorAll('.faq__title');
+    },
+	switchSlide: function (element) {
+		this.elements();
+		this.currentIndex = parseInt(element.dataset.page - 1);
+		element.classList.add('active');
+		element.style.transform = null;
 
-	prepareSlide: function () {
-		slidesIndexTotal = this.slides.length - 1;
-		if(this.currentIndex > slidesIndexTotal) this.currentIndex = slidesIndexTotal;
-		if(this.currentIndex < 0) this.currentIndex = 0;
-		for (let i = 0; i < this.slides.length; i++) {this.slides[i].classList.remove('active');}
-		this.slides[this.currentIndex].classList.add('active');
-		this.slides[this.currentIndex - 1].classList.add('is-left');
-		this.slides[this.currentIndex + 1].classList.add('is-right');
-	},
-
-	switchSlide: function () {
-		slidesIndexTotal = this.slides.length - 1;
-		if(this.currentIndex > slidesIndexTotal) this.currentIndex = slidesIndexTotal;
-		if(this.currentIndex < 0) this.currentIndex = 0;
-
-		for (let i = 0; i < this.slides.length; i++) {this.slides[i].classList.remove('active');}
-		this.slides[this.currentIndex].classList.add('active').classList.remove('is-left', 'is-right');
-		this.slides[this.currentIndex-1].classList.add('is-left').classList.remove('is-active', 'is-right');
-
-	},
-
-	slide: function (element) {
-
-
-		if(element.classList.contains('active')) {
-
-		} else {
-			const page = parseInt(element.dataset.page);
-			const wrapper = element.parentNode;
-			const slides = wrapper.querySelectorAll('.faq__slider-item');
-			for (let i = 0; i < slides.length; i++) {
-				slides[i].classList.remove('active');
-				// if(i === page) {
-				// 	if([i] > slides.length) return;
-				//
-				// 	slides[i+1].style.transform = `translateX(${this.transformBase}%)`;
-				// 	slides[i+2].style.transform = `translateX(${this.transformBase + this.transformIncrement}%)`;
-				// 	slides[i-1].style.transform = `translateX(${-this.transformBase}%)`;
-				// 	slides[i-2].style.transform = `translateX(${-this.transformBase - this.transformIncrement}%)`;
-				// }
-			}
-			element.classList.add('active');
-
+		/* GET PREVIOUS SIBLINGS */
+		for(let i = this.currentIndex - 1; i >= 0; i--) {
+            this.previousCount++;
+            this.slides[i].classList.remove('active');
+            this.slides[i].style.transform = `translateX(-${this.previousCount * this.transformIncrement}%)`;
 		}
-		// const wrapper = element.parentNode;
-		// const slides = wrapper.querySelectorAll('.faq__slider-item');
-		//
-		// for (let i = 0; i < slides.length; i++) {
-		// 	if(slides[i].classList.contains('active')) {
-		// 		for(let y = slides[i]; y < slides.length; i++) {
-		// 			console.log[y];
-		// 		}
-		// 		// slides[i++].style.transform = `translateY(${this.transformBase})`
-		// 	}
-		// }
-	},
+
+        /* GET NEXT SIBLINGS */
+        for(let i = this.currentIndex + 1; i < this.slides.length; i++) {
+            this.nextCount++;
+            this.slides[i].classList.remove('active');
+            this.slides[i].style.transform = `translateX(${this.nextCount * this.transformIncrement}%)`;
+        }
+
+        this.previousCount = 0;
+        this.nextCount = 0;
+    },
 
 	init: function () {
-		for (let i = 0; i < this.slides.length; i++) {
-			this.slides[i].addEventListener('click', (e) => this.slide(this.slides[i].dataset.page));
-		}
+		this.elements();
+		this.slides.forEach(slide => slide.addEventListener('click', () => this.switchSlide(slide)))
 	}
 };
 
