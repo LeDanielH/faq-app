5fa511019bac37233c9d9b7e8ebd24cd75a83344
Daniel <daniel.hoksza@gmail.com>
2017-07-20 03:22:28 +0200

    accordion script update, can open and close

--- devel/assets/_scripts/components/accordion.js
+++ devel/assets/_scripts/components/accordion.js
@@ -1,12 +1,22 @@
-const radios = document.querySelectorAll('input[type="radio"]');
-let radioToBeUnchecked;
-for(let i = 0; i < radios.length; i++) {
-	radios[i].addEventListener('click', function () {
-		if(radioToBeUnchecked === this) {
-			this.checked = false;
-			radioToBeUnchecked = null;
+const Accordion = {
+	elements() {
+		this.accordions = document.querySelectorAll('.faq__accordion-panel');
+	},
+	handler(element) {
+
+		if(element.classList.contains('active')) {
+            element.classList.remove('active')
 		} else {
-			radioToBeUnchecked = this;
+            for(let i = 0; i < this.accordions.length; i++) {
+                this.accordions[i].classList.remove('active');
+            }
+            element.classList.add('active');
 		}
-	})
-}
+	},
+	init: function () {
+		this.elements();
+		this.accordions.forEach(accordion => accordion.addEventListener('click', () => this.handler(accordion)));
+    }
+};
+
+Accordion.init();
