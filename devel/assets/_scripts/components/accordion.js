4557549c490368a6ccc5c31ce4e614bfed528b2f
Daniel <daniel.hoksza@gmail.com>
2017-07-26 22:31:52 +0200

    accordion detect click/touch on header only

--- devel/assets/_scripts/components/accordion.js
+++ devel/assets/_scripts/components/accordion.js
@@ -1,24 +1,24 @@
 const Accordion = {
-	elements() {
-		this.accordions = document.querySelectorAll('.faq__accordion-panel');
-	},
 	handler(element) {
-
-		if(element.classList.contains('active')) {
-            element.classList.remove('active')
+		const accordion = element.parentNode;
+		const isActive = accordion.classList.contains('active');
+		if(isActive) {
+            accordion.classList.remove('active')
 		} else {
             for(let i = 0; i < this.accordions.length; i++) {
                 this.accordions[i].classList.remove('active');
             }
-            element.classList.add('active');
+            accordion.classList.add('active');
 		}
 	},
 	init: function () {
-		this.elements();
+        this.accordionsHeader = document.querySelectorAll('.faq__accordion-header');
+        this.accordions = document.querySelectorAll('.faq__accordion-panel');
 		for(let i = 0; i < this.accordions.length; i++) {
-			this.accordions[i].addEventListener('click',() => this.handler(this.accordions[i]));
+			this.accordionsHeader[i].addEventListener('click',() => this.handler(this.accordionsHeader[i]));
 		}
     }
 };
 
 Accordion.init();
+
