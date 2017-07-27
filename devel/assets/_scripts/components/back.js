7dbc7f062d5dbe8fd846edf4ee2a2c5aa4674a65
Daniel <daniel.hoksza@gmail.com>
2017-07-27 08:10:02 +0200

    removed useless code

--- devel/assets/_scripts/components/back.js
+++
@@ -1,7 +0,0 @@
-function goBack(e) {
-	e.preventDefault();
-	window.history.back();
-}
-
-const backbutton = document.querySelector('.faq__app-back');
-backbutton.addEventListener('click', (e) => goBack(e));
